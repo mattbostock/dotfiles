mattbostock/dotfiles

commit 6c2d5546f636ec4ab7e00c52887b1083237ec2e9

[shell] Add .profile, .zprofile, ssh-agent script
Add a .profile which will read by ZSH emulating a Bourne shell.

Also adds a script to manage `ssh-agent` for systems other than Mac.

profile
@@ -1,0 +1,17 @@
+# ~/.profile: executed by the command interpreter for login shells.
+
+# Set user-defined locale
+export LANG=$(locale -uU)
+
+# This file is not read by bash(1) if ~/.bash_profile or ~/.bash_login
+# exists.
+#
+# if running bash
+if [ -n "${BASH_VERSION}" ]; then
+  if [ -f "${HOME}/.bashrc" ]; then
+    source "${HOME}/.bashrc"
+  fi
+fi
+
+# Initialise ssh-agent
+source .ssh-agent-setup.sh

ssh-agent-setup.sh
@@ -1,0 +1,49 @@
+# Initialise ssh-agent
+# Taken from https://help.github.com/articles/working-with-ssh-key-passphrases
+
+# Note: ~/.ssh/environment should not be used, as it
+#       already has a different purpose in SSH.
+
+env=~/.ssh/agent.env
+
+# Note: Don't bother checking SSH_AGENT_PID. It's not used
+#       by SSH itself, and it might even be incorrect
+#       (for example, when using agent-forwarding over SSH).
+
+agent_is_running() {
+    if [ "$SSH_AUTH_SOCK" ]; then
+        # ssh-add returns:
+        #   0 = agent running, has keys
+        #   1 = agent running, no keys
+        #   2 = agent not running
+        ssh-add -l >/dev/null 2>&1 || [ $? -eq 1 ]
+    else
+        false
+    fi
+}
+
+agent_has_keys() {
+    ssh-add -l >/dev/null 2>&1
+}
+
+agent_load_env() {
+    . "$env" >/dev/null
+}
+
+agent_start() {
+    (umask 077; ssh-agent >"$env")
+    . "$env" >/dev/null
+}
+
+if ! agent_is_running; then
+    agent_load_env
+fi
+
+if ! agent_is_running; then
+    agent_start
+    ssh-add
+elif ! agent_has_keys; then
+    ssh-add
+fi
+
+unset env

zprofile
@@ -1,0 +1,2 @@
+# Use .profile if it exists
+[[ -e ~/.profile ]] && emulate sh -c 'source ~/.profile'

commit 235be0e3b017326e13ab8361cf50d62dcc10b878

Don't set LANG env var in .profile
Remove line that attempts to sent the `LANG` environment variable from
the output of `locale`.

This was introduced accidentally in
6c2d5546f636ec4ab7e00c52887b1083237ec2e9. The `-Uu` commandline options
passed to `locale` are not compatible with Mac OS X.

profile
@@ -1,8 +1,5 @@
 # ~/.profile: executed by the command interpreter for login shells.
 
-# Set user-defined locale
-export LANG=$(locale -uU)
-
 # This file is not read by bash(1) if ~/.bash_profile or ~/.bash_login
 # exists.
 #